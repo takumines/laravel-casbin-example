0edbfd26082a3c834583058fa1bd3bf44e6624d1
takumines <takumines1@gmail.com>
2025-01-08 22:50:54 +0900

    [update] 認証関連のルーティング設定変更

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,36 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Article\DestroyController;
+use App\Http\Controllers\Article\IndexController;
+use App\Http\Controllers\Article\ShowController;
+use App\Http\Controllers\Article\StoreController as ArticleStoreController;
+use App\Http\Controllers\Article\UpdateController;
+use App\Http\Controllers\Comment\StoreController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health-check', function () {
-    return response()->json([
-        'status' => 'ok',
-    ]);
+// 記事一覧
+Route::get('/articles', [IndexController::class])->name('articles.index');
+// 記事詳細
+Route::get('/articles/{article}', [ShowController::class])->name('articles.show');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    // 記事作成
+    Route::post('/articles', [ArticleStoreController::class])->name('articles.store');
+    // 記事更新
+    Route::put('/articles/{article}', [UpdateController::class])->name('articles.update');
+    // 記事削除
+    Route::delete('/articles/{article}', [DestroyController::class])->name('articles.delete');
+    // コメント作成
+    Route::post('/articles/{article}/comments', [StoreController::class])->name('comments.store');
+    // コメント削除
+    Route::delete('/articles/{article}/comments/{comment}', [DestroyController::class])
+        ->name('comments.delete');
+    // ユーザー一覧
+    Route::get('/users', [IndexController::class])->name('users.index');
+    // ユーザー詳細
+    Route::get('/users/{user}', [ShowController::class])->name('users.show');
+    // ユーザー削除
+    Route::delete('/users/{user}', [DestroyController::class])->name('users.delete');
 });
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+require __DIR__.'/auth.php';
